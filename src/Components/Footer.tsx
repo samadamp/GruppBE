ab245968e9d6d9e6a6c45c5d136c1f6deaa43026
samadamp <94sampan@skola.boras.se>
2024-10-18 11:15:45 +0200

    lol

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-      <footer className=" bg-Primary text-bookFlix-colors-detail p-4 mt-10 text-white">
+      <footer className=" bg-Primary p-4 mt-10 w-full text-white">
         <p className="container mx-auto text-center">
           &copy; 2024, All rights reserved.
         </p>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,14 @@
-import { Testing } from "../test";
+
 import NavBAr from "../Components/NavBar";
 import Footer from "../Components/Footer";
+
 
 export default function HomePage() {
     return (
         <div className="flex flex-col min-h-screen  bg-Background">
             <NavBAr />
         <main className="flex-grow flex flex-col justify-center items-center  bg-Background">
-            <Testing />
+        <img src="/lol.png" alt="Lol Image" className="w-50% h-50%" />
             <h1>Home Page</h1>
             <Footer />
             </main>
