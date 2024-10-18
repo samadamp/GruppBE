db94c9361b14d8d454b69e8fd787c0cf86969109
samadamp <94sampan@skola.boras.se>
2024-10-18 10:27:35 +0200

    add tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import { Testing } from './test'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
+    <Testing />
   </StrictMode>,
 )
