9946193c65e3c89c7f4141097777922649cca3b6
samadamp <94sampan@skola.boras.se>
2024-10-18 11:04:47 +0200

    Event page

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import EventPage from './pages/EventPage'
 import NotFoundPage from './pages/NotFoundPage'
 import './index.css'
+
 
 const router = createBrowserRouter([{
   path: '/',
   element: <HomePage />,
+  errorElement: <NotFoundPage />,
+},
+{
+  path: '/Eventpage',
+  element: <EventPage />,
   errorElement: <NotFoundPage />,
 },
 ]);
